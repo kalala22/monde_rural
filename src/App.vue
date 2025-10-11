639a3b5b8b8d16d413f6d8e787b0e018d2a14f2c
Chretien Kalala <chretienkalala22@gmail.com>
2025-10-11 15:43:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <nav class=""></nav>
+  </header>
+
+  <main></main>
+</template>
