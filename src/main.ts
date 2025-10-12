29c46b5cd406aac370ee264753dcb5e06cfc71c3
Chretien Kalala <chretienkalala22@gmail.com>
2025-10-12 17:53:19 +0100

    fix bug affichage navbar vue mobile

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,42 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+  faX,
+  faBars,
+  faTimes,
+  faEnvelope,
+  faDownload,
+  faCode,
+  faServer,
+  faPalette,
+  faCamera,
+  faImage,
+  faTasks,
+  faArrowUpRightFromSquare,
+  faArrowUp,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faX,
+  faBars,
+  faTimes,
+  faEnvelope,
+  faGithub,
+  faDownload,
+  faCode,
+  faEnvelope,
+  faServer,
+  faPalette,
+  faCamera,
+  faImage,
+  faTasks,
+  faArrowUpRightFromSquare,
+  faArrowUp,
+)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
