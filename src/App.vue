a90b10163ed396a49cf026bf81cc57e0bc5ce45e
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-01 15:52:43 +0100

    add AppView

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
 import Navbar from '@/components/NavbarView.vue'
+import HomeView from '@/components/HomeView.vue'
 </script>
 
 <template>
   <div>
     <Navbar />
+    <HomeView />
   </div>
 </template>
