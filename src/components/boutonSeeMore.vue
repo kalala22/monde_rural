0d85130e2c6b7220295bad7f321090e7e481d846
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-01 15:49:21 +0100

    create button see more

---
+++ src/components/boutonSeeMore.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Link {
+  name: string
+  href: string
+}
+import { ref } from 'vue'
+const links = ref<Link[]>([{ name: 'En savoir plus', href: '#' }])
+</script>
+<template>
+  <a
+    v-for="link in links"
+    :key="link.name"
+    :href="link.href"
+    class="bg-transparent shadow-sm flex items-center justify-center transition duration-600 ease-in-out gap-1 hover:bg-vertrural/90 text-black hover:text-white font-bold text-lg px-4 py-2 rounded-lg"
+  >
+    {{ link.name }}
+    <font-awesome-icon icon="" />
+  </a>
+</template>
