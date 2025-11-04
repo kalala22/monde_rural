8dc9243cfd50ac4262c70399696703945fd101a4
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-04 22:40:02 +0100

    add boutonAgament component to display engagement links with icons

---
+++ src/components/boutonAgament.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Link {
+  name: string
+  href: string
+  icon?: string
+}
+import { ref } from 'vue'
+const links = ref<Link[]>([{ name: "Je m'engage", href: '#', icon: 'arrow-right' }])
+</script>
+<template>
+  <a
+    v-for="link in links"
+    :key="link.name"
+    :href="link.href"
+    class="bg-blue-rural shadow-sm flex items-center justify-center transition duration-600 ease-in-out gap-1 font-bold text-lg px-4 py-2 rounded-lg hover:scale-105 text-white"
+  >
+    {{ link.name }}
+    <font-awesome-icon :icon="link.icon" />
+  </a>
+</template>
