94f596eab25395bece163088c8a11ec5a7e08d91
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-04 23:31:06 +0100

    update HomeView.vue to enhance text visibility and background contrast

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -8,18 +8,20 @@ import BoutonSeeMore from './boutonSeeMore.vue'
       class="relative flex items-center justify-center bg-no-repeat bg-cover bg-center bg-[url(src/assets/img/bgMondeRuralAgricol.png)] h-screen"
     >
       <!-- Ajout de la surcouche avec opacité -->
-      <div class="absolute inset-0 bg-white opacity-40"></div>
+      <div class="absolute inset-0 bg-black opacity-40"></div>
 
       <!-- Assurez-vous que votre contenu est au-dessus de la surcouche -->
       <div class="relative max-w-4xl mx-4 text-center">
         <div class="inline-flex items-center gap-2 bg-vertrural/10 px-4 py-2 rounded-full mb-6">
           <!-- <Leaf class="h-5 w-5 text-vertrural" /> -->
           <font-awesome-icon icon="leaf" class="h-5 w-5 text-vertrural" />
-          <span class="text-sm font-bold text-vertrural">Développement Équitable et Durable</span>
+          <span class="text-sm font-bold text-vertrural animate-pulse"
+            >Développement Équitable et Durable</span
+          >
         </div>
         <div>
           <h1
-            class="font-roboto font-bold text-4xl md:text-6xl mb-8 text-balance"
+            class="font-roboto font-bold text-4xl md:text-6xl mb-8 text-balance text-white"
             v-motion
             :initial="{ opacity: 0, y: 100 }"
             :visible="{ opacity: 1, y: 0 }"
@@ -28,7 +30,7 @@ import BoutonSeeMore from './boutonSeeMore.vue'
           </h1>
         </div>
         <blockquote
-          class="text-xl md:text-2xl font-bold italic text-secondary mb-6"
+          class="text-xl md:text-2xl font-bold italic text-white mb-6"
           v-motion
           :initial="{ opacity: 0, y: 100 }"
           :visible="{ opacity: 1, y: 0 }"
@@ -37,7 +39,7 @@ import BoutonSeeMore from './boutonSeeMore.vue'
         </blockquote>
 
         <p
-          class="text-lg md:text-xl text-muted-foreground mb-10 text-balance"
+          class="text-lg md:text-xl text-muted-foreground mb-10 text-balance text-white"
           v-motion
           :initial="{ opacity: 0, y: 100 }"
           :visible="{ opacity: 1, y: 0 }"
