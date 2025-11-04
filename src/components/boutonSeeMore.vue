e438cb4dd7c83fc4083d7c69c4511af7f88eff83
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-04 23:31:26 +0100

    update boutonSeeMore.vue to improve text color for better visibility

--- src/components/boutonSeeMore.vue
+++ src/components/boutonSeeMore.vue
@@ -11,7 +11,7 @@ const links = ref<Link[]>([{ name: 'En savoir plus', href: '#' }])
     v-for="link in links"
     :key="link.name"
     :href="link.href"
-    class="bg-transparent shadow-sm flex items-center justify-center transition duration-600 ease-in-out gap-1 hover:bg-vertrural/90 text-black hover:text-white font-bold text-lg px-4 py-2 rounded-lg"
+    class="bg-transparent shadow-sm flex items-center justify-center transition duration-600 ease-in-out gap-1 hover:bg-vertrural/90 text-white hover:text-white font-bold text-lg px-4 py-2 rounded-lg"
   >
     {{ link.name }}
     <font-awesome-icon icon="" />
