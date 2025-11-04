4aa37111b9d125b4182c59c780941e9a86cc95f8
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-04 20:06:46 +0100

    add SectionAxeView component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import Navbar from '@/components/NavbarView.vue'
 import HomeView from '@/components/HomeView.vue'
+import SectionAxeView from './components/SectionAxeView.vue'
 </script>
 
 <template>
   <div>
     <Navbar />
     <HomeView />
+    <SectionAxeView />
   </div>
 </template>
