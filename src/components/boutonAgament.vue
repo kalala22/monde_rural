e787a2eccc1eba02d6bfecb8c83e0387cbd3274b
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-05 13:16:13 +0100

    update boutonAgament.vue to change button background color for improved theming

--- src/components/boutonAgament.vue
+++ src/components/boutonAgament.vue
@@ -12,7 +12,7 @@ const links = ref<Link[]>([{ name: "Je m'engage", href: '#', icon: 'arrow-right'
     v-for="link in links"
     :key="link.name"
     :href="link.href"
-    class="bg-blue-rural shadow-sm flex items-center justify-center transition duration-600 ease-in-out gap-1 font-bold text-lg px-4 py-2 rounded-lg hover:scale-105 text-white"
+    class="bg-vertrural shadow-sm flex items-center justify-center transition duration-600 ease-in-out gap-1 font-bold text-lg px-4 py-2 rounded-lg hover:scale-105 text-white"
   >
     {{ link.name }}
     <font-awesome-icon :icon="link.icon" />
