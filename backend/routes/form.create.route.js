ebf15a18f412307e6d0fb6302527bc66fbb1fb58
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-12 12:09:32 +0100

    add form creation route with validation rules

---
+++ backend/routes/form.create.route.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { Router } = require('express')
+const { body, validationResult } = require('express-validator')
+const { postCreateForm } = require('../controllers/post.form')
+const router = Router()
+
+const validationRules = [
+  // Le nom ne doit pas être vide
+  body('nom').trim().notEmpty().withMessage('Le nom est obligatoire.'),
+
+  // L'email doit être un email valide
+  body('email')
+    .trim() // Enlève les espaces inutiles au début et à la fin
+    .isEmail()
+    .withMessage('Veuillez fournir une adresse email valide.')
+    .normalizeEmail(), // Convertit l'email en format standard (ex: GMail.User@... -> gmail.user@...)
+
+  // Le type d'adhésion doit être l'une des valeurs autorisées
+  body('membershipType')
+    .isIn(['membre', 'partenaire', 'bénévole'])
+    .withMessage("Le type d'adhésion est invalide."),
+]
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    // S'il y a des erreurs, renvoyer une réponse 400 avec les détails
+    return res.status(400).json({ errors: errors.array() })
+  }
+  // S'il n'y a pas d'erreurs, passer au contrôleur suivant
+  next()
+}
+router.post('/send-form', validationRules, handleValidationErrors, postCreateForm)
+
+module.exports = router
