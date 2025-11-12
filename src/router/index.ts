74f1a83c8d04985907c63ff23e58a1970d1e40e4
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-12 12:13:30 +0100

    add admin routes with authentication meta for Connexion and Dashboard views

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/pages/HomePageView.vue'
 import AboutPageView from '@/pages/AboutPageView.vue'
 import JoinUsPageView from '@/pages/JoinUsPageView.vue'
+import DashbordInterfaceView from '@/pages/DashbordInterfaceView.vue'
+import ConnexionView from '@/pages/ConnexionView.vue'
 const router = createRouter({
   routes: [
     {
@@ -16,8 +18,35 @@ const router = createRouter({
       path: '/join-us',
       component: JoinUsPageView,
     },
+    {
+      path: '/admin',
+
+      component: ConnexionView,
+      meta: { hideLayout: true, requiresAuth: true }, // Ajout du champ méta
+    },
+    {
+      path: '/admin/dashboard',
+
+      component: DashbordInterfaceView,
+      meta: { hideLayout: true, requiresAuth: true }, // Ajout du champ méta
+    },
   ],
   history: createWebHashHistory(),
 })
 
+// router.beforeEach((to, from, next) => {
+//   const isAuthenticated = !!localStorage.getItem('authToken')
+
+//   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
+
+//   // LOGIQUE DE REDIRECTION
+
+//   if (requiresAuth && !isAuthenticated) {
+//     next({ name: 'admin' })
+//   } else if (to.name === 'admin' && isAuthenticated) {
+//     next({ name: 'dashboard' })
+//   } else {
+//     next()
+//   }
+// })
 export default router
