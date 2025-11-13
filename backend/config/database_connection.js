5e3e1c04a872d2ebc3746b67cf9b1b3a38e3a2ce
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-13 11:30:17 +0100

    format code

--- backend/config/database_connection.js
+++ backend/config/database_connection.js
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const dotenv = require('dotenv')
+import dotenv from 'dotenv'
+import { Sequelize } from 'sequelize'
+
 dotenv.config()
-const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'mysql',
   timezone: '+01:00',
 })
+
 ;(async () => {
   try {
     await sequelize.authenticate()
@@ -18,4 +19,5 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     console.error('Unable to connect to the database:', error)
   }
 })()
-module.exports = sequelize
+
+export default sequelize
