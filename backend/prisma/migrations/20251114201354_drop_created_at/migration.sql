78e7dc99838bbbc691ebd7c782c7fe494f445cfa
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-15 02:17:38 +0100

    refactor(database): migrate from MySQL to PostgreSQL and update schema

--- backend/prisma/migrations/20251114201354_drop_created_at/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `users` DROP COLUMN `createdAt`,
-    MODIFY `date_adhesion` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0);
